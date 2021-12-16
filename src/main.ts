8ec08a0e020ac3482708763ad5a1f88c3b0f6b3e
AlexEvs <evstafiadi.shura@yandex.ru>
2021-12-16 21:38:32 +0300

    CONTROLLER AND GET USERS METHOD

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
-import {NestFactory} from "@nestjs/core"
+import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
-async function start()
-{
-    const PORT = process.env.PORT || 5000;
-    const app = await NestFactory.create(AppModule);
+async function start() {
+  const PORT = process.env.PORT || 5000;
+  const app = await NestFactory.create(AppModule);
 
-    await app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+  await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
 
 start();
