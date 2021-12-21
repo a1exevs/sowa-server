2b5f4799c8ad07ddbd0a521c786ed99771caa6ee
AlexEvs <evstafiadi.shura@yandex.ru>
2021-12-21 21:32:16 +0300

    /users GET and POST methods, POSTMAN-COLLECTIONS

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { CreateUserDto } from "./DTO/CreateUser";
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  create(@Body() dto: CreateUserDto) {
+    return this.usersService.createUser(dto);
+  }
+
+  @Get()
+  getAll() {
+    return this.usersService.getAllUsers();
+  }
+}
