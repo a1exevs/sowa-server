45ee8ab1a2db3a9540a908c661525b823ac3398b
AlexEvs <evstafiadi.shura@yandex.ru>
2021-12-21 21:50:13 +0300

    Docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+
+  const docConfig = new DocumentBuilder()
+    .setTitle("SOWA-Server")
+    .setDescription("This is an API for messengers SOWA")
+    .setVersion("1.0.0")
+    .addTag("SOWA JS")
+    .build();
+  const document = SwaggerModule.createDocument(app, docConfig);
+  SwaggerModule.setup('/api/docs', app, document);
 
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
