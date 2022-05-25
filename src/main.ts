65f8a8770fe6e8f40111b0d267668a11534edea5
AlexEvs <evstafiadi.shura@yandex.ru>
2022-05-25 23:12:08 +0300

    Delete dir "api"

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import { ValidationPipe } from "./api/common/pipes/validation.pipe";
+import { ValidationPipe } from "./common/pipes/validation.pipe";
 import * as cookieParser from 'cookie-parser';
-import {Logger} from "./api/common/logs/Logger";
+import {Logger} from "./common/logs/Logger";
 import {INestApplication} from "@nestjs/common";
 import * as session from 'express-session';
 
