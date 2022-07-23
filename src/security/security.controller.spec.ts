c43801a6a1275419246fa0636b1481ff7f3c61c2
AlexEvs <evstafiadi.shura@yandex.ru>
2022-07-23 19:48:24 +0300

    Added tests for Security end point

---
+++ src/security/security.controller.spec.ts
@@ -0,0 +1,59 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { SecurityController } from "./security.controller";
+import { SecurityService } from "./security.service";
+import { ResponseInterceptor } from "../common/interceptors/ResponseInterceptor";
+import { createRequest } from "node-mocks-http";
+
+describe('SecurityController', () => {
+  let securityController: SecurityController;
+  let securityService: SecurityService;
+  let responseInterceptor: ResponseInterceptor<any>;
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      controllers: [SecurityController],
+      providers: [
+        {
+          provide: SecurityService,
+          useValue: {
+            getCaptchaURL: jest.fn(x => x)
+          }
+        }
+      ]
+    }).compile();
+    securityController = moduleRef.get<SecurityController>(SecurityController);
+    securityService = moduleRef.get<SecurityService>(SecurityService);
+    responseInterceptor = moduleRef.get<ResponseInterceptor<any>>(ResponseInterceptor);
+  });
+
+  describe('SecurityController - definition', () => {
+    it('SecurityController - should be defined', () => {
+      expect(securityController).toBeDefined();
+    });
+    it('SecurityService - should be defined', () => {
+      expect(securityService).toBeDefined();
+    });
+    it('ResponseInterceptor - should be defined', () => {
+      expect(responseInterceptor).toBeDefined();
+    });
+  });
+
+  describe('SecurityController - getCaptchaURL', () => {
+    it('should be successful result', async () => {
+      const request = createRequest();
+      const captchaURL = 'sawa.com/captcha';
+      const captchaText = '1234';
+      request._setSessionVariable('captcha', '');
+      jest.spyOn(securityService, 'getCaptchaURL').mockImplementation(() => {
+        return Promise.resolve({ captchaURL, captchaText })
+      })
+      const result = await securityController.getCaptchaURL(request);
+
+      expect(result.captchaURL).toBe(captchaURL);
+      expect(request.session['captcha']).toBe(captchaText);
+      expect(securityService.getCaptchaURL).toBeCalledTimes(1);
+    })
+  })
+});
---
+++ src/security/security.service.spec.ts
@@ -0,0 +1,64 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { SecurityService } from "./security.service";
+import { FilesService } from "../files/files.service";
+import * as svgCaptcha from 'svg-captcha'
+
+jest.mock('svg-captcha')
+
+describe('SecurityService', () => {
+  let securityService: SecurityService;
+  let filesService: FilesService;
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        SecurityService,
+        {
+          provide: FilesService,
+          useValue: {
+            createFile: jest.fn(x => x),
+            deleteFileWithTimer: jest.fn(x => x)
+          }
+        }
+      ]
+    }).compile();
+    securityService = moduleRef.get<SecurityService>(SecurityService);
+    filesService = moduleRef.get<FilesService>(FilesService);
+  });
+
+  describe('SecurityService - definition', () => {
+    it('SecurityService - should be defined', () => {
+      expect(securityService).toBeDefined();
+    });
+    it('FilesService - should be defined', () => {
+      expect(filesService).toBeDefined();
+    });
+  });
+
+  describe('SecurityService - getCaptchaURL', () => {
+    it('should be successful result', async () => {
+      const captchaText = '1234';
+      const captchaData = 'svg-data';
+      const filePath = 'filePath';
+      const fileURL = 'fileURL';
+      const captcha = { text: captchaText, data: captchaData };
+      jest.spyOn(svgCaptcha, 'create').mockImplementation(() => {
+        return captcha;
+      })
+      jest.spyOn(filesService, 'createFile').mockImplementation(() => {
+        return Promise.resolve({ filePath, fileURL })
+      })
+      const result = await securityService.getCaptchaURL();
+      expect(result.captchaText).toBe(captchaText);
+      expect(result.captchaURL).toBe(fileURL);
+      expect(svgCaptcha.create).toBeCalledTimes(1);
+      expect(svgCaptcha.create).toReturnWith(captcha);
+      expect(filesService.createFile).toBeCalledTimes(1);
+      expect(filesService.createFile).toBeCalledWith({ buffer: captchaData }, '', 'svg','security/captcha');
+      expect(filesService.deleteFileWithTimer).toBeCalledTimes(1);
+      expect(filesService.deleteFileWithTimer).toBeCalledWith(filePath, 10);
+    })
+  })
+});
